1e1f8d08c1e62894b670a56efdf9eaef0432e1e6
Chris Ward <cward@extron.com>
2021-06-13 02:47:29 -0400

    Fix undefined when account is not in list

--- aws-fix-role.js
+++ aws-fix-role.js
@@ -20,13 +20,16 @@
     const accountId = document.querySelectorAll("[data-testid='aws-my-account-details']")[0].innerText;
     let len = elms.length;
 
+    const accountName = accountNameMap.get(accountId);
+    const accountNameDisplay = accountName ? ` - ${accountName}` : '';
+
     for(let ii = 0; ii < len; ii++) {
         let myChildren = elms[ii].childNodes;
         let len2 = myChildren.length;
         for (let jj = 0; jj < len2; jj++) {
             if(myChildren[jj].nodeType === 3) {
                 if (myChildren[jj].nodeValue.includes("AWSReservedSSO_")){
-                    myChildren[jj].nodeValue = `${myChildren[jj].nodeValue.split('_')[1]} - ${accountNameMap.get(accountId)}`;
+                    myChildren[jj].nodeValue = `${myChildren[jj].nodeValue.split('_')[1]} ${accountNameDisplay}`;
                 }
             }
         }
